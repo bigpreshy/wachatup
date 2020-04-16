34d9caed96dffa1f8ca4d429c4bdcfc30096974b
Preshy <lukeprecious@gmail.com>
2020-04-16 04:47:43 +0100

    work dynamically

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
 
     $('.num').click(function () {
+    	
         var num = $(this);
         var text = $.trim(num.find('.txt').clone().children().remove().end().text());
         var telNumber = $('#telNumber');
@@ -11,3 +12,10 @@ $(document).ready(function () {
 
 });
 
+function myFunction() {
+  var api = "https://wa.me/";
+  var x = document.getElementById("test").elements[0].value; 
+  var what = api.concat(x);
+  window.location.href = what;
+  document.getElementById("root").innerHTML = what;
+}
